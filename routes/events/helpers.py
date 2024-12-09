4bd27ff7d51ae1b7d5e67dbbf5e68550a7e70921
nathanfenwick <nathanfenwick@rocketmail.com>
2024-12-09 13:34:32 +0000

    Added stub functions for each of the request check to add.

--- routes/events/helpers.py
+++ routes/events/helpers.py
@@ -14,3 +14,19 @@ def validate_amount(current_amount):
         return float_amount
     except (ValueError, AttributeError):
         raise ValueError("The amount must be a positive number with exactly two decimal places (e.g., '42.00').")
+
+
+def check_single_large_withdrawal():
+    pass
+
+
+def check_three_consecutive_withdrawals():
+    pass
+
+
+def check_three_consecutive_larger_deposits():
+    pass
+
+
+def check_deposit_limit_exceeded_in_window():
+    pass
